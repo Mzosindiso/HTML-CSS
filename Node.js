aceaaf96f19ee1ca6b9a5a2e2c7e7c1f147011c5
Mzosindiso <80448819+Mzosindiso@users.noreply.github.com>
2024-09-19 15:31:39 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,1 +1,7 @@
+# Node
+//Creating a new project commands
+mkdir my-node-project //Project creation.
+cd my-node-project //Directory.
+npm init -y //Initialiaze (creates a package.json file in your project directory).
+npm install express // installs any required packages for your project.
 
